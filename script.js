64a93396fe74b940ec1cb7a8ecf15e8f54516ce6
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-05 10:54:06 -0500

    x button

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 $(document).ready(function() {
   // Function to add a new item
   function addItem() {
-      let inputValue = $('#input').val().trim();
+      var inputValue = $('#input').val().trim();
       if (inputValue === '') {
-          // Handle empty input without an alert if desired
-          return; // Exit the function early if no input
+          // Optionally handle empty input without an alert
+      } else {
+          // Create a new list item and append the input value
+          var li = $('<li></li>');
+          li.text(inputValue);
+
+          // Create a delete button with inline styles to ensure visibility
+          var deleteButton = $('<span class="delete">X</span>').css({
+              'display': 'inline', // Override CSS to ensure it's visible
+              'margin-left': '5px', // Add some space from the to-do item text
+              'cursor': 'pointer' // Change cursor on hover
+          });
+
+          // Append the delete button to the list item
+          li.append(deleteButton);
+
+          // Append the list item to the list
+          $('#list').append(li);
+
+          // Clear the input field
+          $('#input').val('');
+
+          // Event handler for the delete button
+          deleteButton.click(function() {
+              $(this).parent().fadeOut('slow', function() {
+                  $(this).remove(); // Remove the item from the DOM
+              });
+          });
+
+          // Event handler for striking through the item on double click
+          li.dblclick(function() {
+              $(this).toggleClass('strike');
+          });
       }
-      // Create a list item with the input text and a delete button
-      let li = $('<li></li>');
-      li.append(document.createTextNode(inputValue));
-      let deleteButton = $('<span class="delete">X</span>');
-      li.append(deleteButton);
-
-      // Append the new list item to the list
-      $('#list').append(li);
-
-      // Clear the input field
-      $('#input').val('');
-
-      // Click event for the delete button
-      deleteButton.click(function() {
-          $(this).parent().remove();
-      });
-
-      // Double-click event to toggle the strike-through
-      li.dblclick(function() {
-          $(this).toggleClass('strike');
-      });
   }
 
   // Event listener for the 'Add' button
   $('#button').click(addItem);
 
   // Event listener for pressing the 'Enter' key in the input field
-  $('#input').on('keypress', function(e) {
-      if (e.which === 13) {
+  $('#input').keypress(function(e) {
+      if (e.which == 13) { // Enter key
           e.preventDefault();
           addItem();
       }
