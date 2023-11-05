1d08695814dab708ccb5189e84377241066bbcbd
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-05 10:44:59 -0500

    deleteathon

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 $(document).ready(function() {
   // Function to add a new item
-  function newItem() {
-      var inputValue = $('#input').val().trim();
-      if (inputValue !== '') {
-          // Create the list item with the delete button as a string
-          var listItem = '<li>' + inputValue + ' <span class="delete" style="display: inline;">X</span></li>';
-          
-          // Append the new list item to the list
-          $('#list').append(listItem);
-          
+  function addItem() {
+      let inputValue = $('#input').val().trim();
+      if (inputValue === '') {
+          alert("You must write something!"); // Or handle this silently as you prefer
+      } else {
+          // Create the list item with text
+          let li = $('<li></li>').text(inputValue);
+          // Create the delete button
+          let deleteButton = $('<span class="delete">X</span>');
+
+          // Append the delete button to the list item
+          li.append(deleteButton);
+          // Append the list item to the list
+          $('#list').append(li);
           // Clear the input field
-          $('#input').val("");
+          $('#input').val('');
       }
   }
 
-  // Event listener for adding an item with the Add button
-  $('#button').click(newItem);
+  // Function to cross out a list item
+  function crossOut() {
+      $(this).toggleClass("strike");
+  }
 
-  // Event listener for adding an item with the Enter key
-  $("#input").on('keypress', function(e) {
-      if (e.which === 13) {
-          e.preventDefault(); // Prevent the default action for Enter key
-          newItem();
-      }
-  });
-
-  // Event listener for marking an item as complete
-  $('#list').on('dblclick', 'li', function() {
-      $(this).toggleClass('strike');
-  });
-
-  // Event listener for deleting an item
-  $('#list').on('click', '.delete', function() {
+  // Function to delete a list item
+  function deleteListItem() {
       $(this).parent().fadeOut('slow', function() {
           $(this).remove();
       });
+  }
+
+  // Event listener for the 'Add' button
+  $('#button').click(addItem);
+
+  // Event listener for pressing the 'Enter' key in the input field
+  $('#input').on('keypress', function(e) {
+      if (e.which === 13) {
+          e.preventDefault();
+          addItem();
+      }
   });
 
-  // Make the list sortable with drag and drop
+  // Event delegation for crossing out an item on double-click
+  $('#list').on('dblclick', 'li', crossOut);
+
+  // Event delegation for deleting an item on click of the delete button
+  $('#list').on('click', '.delete', deleteListItem);
+
+  // Make the list sortable
   $('#list').sortable();
 });
