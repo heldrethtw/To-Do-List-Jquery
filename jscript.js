f7f84cbcd3951cf8626b6e59ef6c8a3a85b28a35
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-05 08:58:36 -0500

    js/jq

---
+++ jscript.js
@@ -0,0 +1,39 @@
+function newItem() {
+    // 1. Adding a new item to the list of items:
+    var inputValue = $('#input').val();
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      // Create the list item with text and append it to the list
+      var li = $('<li></li>').text(inputValue);
+      $('#list').append(li);
+      $('#input').val('');
+  
+      // 2. Crossing out an item from the list of items:
+      li.dblclick(function() {
+        $(this).toggleClass("strike");
+      });
+  
+      // 3. Adding the delete button "X":
+      var deleteButton = $('<button></button>').text('X').addClass('delete');
+      li.append(deleteButton);
+  
+      // Event handler for the delete button
+      deleteButton.click(function() {
+        $(this).parent().remove();
+      });
+    }
+  
+    // 4. Reordering the items:
+    $('#list').sortable();
+  }
+  
+  // Event listener for the Add button
+  $('#button').click(newItem);
+  
+  // Prevent form submission and trigger adding new item
+  $('form[name="toDoList"]').submit(function(e) {
+    e.preventDefault();
+    newItem();
+  });
+  
