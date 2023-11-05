1e8b98dd28df7d1e9b3ed25d4c82a91ccde1f6b9
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-11-05 10:56:58 -0500

    using CF code

--- script.js
+++ script.js
@@ -1,55 +1,30 @@
-$(document).ready(function() {
-  // Function to add a new item
-  function addItem() {
-      var inputValue = $('#input').val().trim();
-      if (inputValue === '') {
-          // Optionally handle empty input without an alert
-      } else {
-          // Create a new list item and append the input value
-          var li = $('<li></li>');
-          li.text(inputValue);
+// jQuery Code
+//1. Adding a new item to the list:
 
-          // Create a delete button with inline styles to ensure visibility
-          var deleteButton = $('<span class="delete">X</span>').css({
-              'display': 'inline', // Override CSS to ensure it's visible
-              'margin-left': '5px', // Add some space from the to-do item text
-              'cursor': 'pointer' // Change cursor on hover
-          });
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
 
-          // Append the delete button to the list item
-          li.append(deleteButton);
+if (inputValue === '') {
+  alert("You must write something!");
+} else {
+  $('#list').append(li);
+}
+//2. Crossing an item out:
+function crossOut() {
+    li.toggleClass("strike");
+}
 
-          // Append the list item to the list
-          $('#list').append(li);
-
-          // Clear the input field
-          $('#input').val('');
-
-          // Event handler for the delete button
-          deleteButton.click(function() {
-              $(this).parent().fadeOut('slow', function() {
-                  $(this).remove(); // Remove the item from the DOM
-              });
-          });
-
-          // Event handler for striking through the item on double click
-          li.dblclick(function() {
-              $(this).toggleClass('strike');
-          });
-      }
-  }
-
-  // Event listener for the 'Add' button
-  $('#button').click(addItem);
-
-  // Event listener for pressing the 'Enter' key in the input field
-  $('#input').keypress(function(e) {
-      if (e.which == 13) { // Enter key
-          e.preventDefault();
-          addItem();
-      }
-  });
-
-  // Make the list sortable
-  $('#list').sortable();
+li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
 });
+//3. Adding a delete button
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+function deleteListItem(){
+    li.addClass("delete")
+}
+$('#list').sortable();
